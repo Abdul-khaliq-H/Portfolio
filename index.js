bb4510a70b0193f4245d7f8dbf210d063d57fef5
Abdul Khaliq H <abdulkhaliq06122003@gmail.com>
2024-07-04 15:46:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+
+// Skill transitioin
+document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.querySelectorAll('.cardCus');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('in-view');
+            }
+        });
+    });
+
+    cards.forEach(card => {
+        observer.observe(card);
+    });
+});
+
+// JavaScript (Vanilla)
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach(function(navLink) {
+        navLink.addEventListener("click", function(event) {
+            // Remove active class from all nav links
+            navLinks.forEach(function(link) {
+                link.classList.remove("active");
+            });
+
+            // Add active class to the clicked nav link
+            this.classList.add("active");
+        });
+    });
+});
+
+// jQuery
+
+$(document).ready(function() {
+    $(".nav-link").on("click", function() {
+        // Remove active class from all nav links
+        $(".nav-link").removeClass("active");
+        
+        // Add active class to the clicked nav link
+        $(this).addClass("active");
+    });
+
+    // Handling the certifications button separately
+    $("#certificationsBtn").on("click", function() {
+        // Add active class to the button
+        $(this).toggleClass("active");
+    });
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message Sent!');
+});
+
+
+
+
+
+
